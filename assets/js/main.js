c012bcf42c89819ffae04893b2e995c549cd9a7e
Shiela mae Rafols Malaque <Shielamaemalaque@gmail.com>
2023-05-29 22:26:45 +0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+    $('.slick').slick({
+        dots: false,
+        infinite: true,
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        cssEase: 'linear',
+        nextArrow: '<button class="slick-next px-2 py-1 bg-transparent"><i class="bi bi-chevron-right text-white next"></i></button>',
+        prevArrow: '<button class="slick-prev px-2 py-1 bg-transparent"><i class="bi bi-chevron-left text-white prev"></i></button>',
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 412,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+document.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav');
+    if(window.scrollY > 0)
+    {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled')
+    }
+})
