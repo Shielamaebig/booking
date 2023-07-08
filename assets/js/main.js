94a6636a1ca4cc4664b506cf91f55dcc30c86dba
Shiela mae Rafols Malaque <Shielamaemalaque@gmail.com>
2023-07-08 11:10:23 +0800

    -Rooms

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,7 +57,56 @@ $(document).ready(function () {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
-      });
+    });
+
+
+    $('.slick-room').slick({
+        dots: false,
+        infinite: true,
+      touchThreshold : 100,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      centerMode: true,
+      nextArrow: '<button class="slick-next px-2  bg-transparent"><i class="bi bi-chevron-right text-white next"></i></button>',
+      prevArrow: '<button class="slick-prev px-2  bg-transparent"><i class="bi bi-chevron-left text-white prev"></i></button>',
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 412,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
 
 document.addEventListener('scroll', () => {
@@ -69,3 +118,7 @@ document.addEventListener('scroll', () => {
         nav.classList.remove('scrolled')
     }
 })
+
+
+
+
